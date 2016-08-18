d608daa376bb2e0b50d885105d10b62e7d86eb76
eflorespalma <eflorespalma@gmail.com>
2016-08-18 16:22:02 -0500

    Initial Commit

---
+++ Client/App_Start/AutofacConfig.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Integration.Wcf;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using Server;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace Client.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.Register(c => new ChannelFactory<IService>(new BasicHttpBinding(), new EndpointAddress("http://localhost:58296/Service.svc"))).SingleInstance();
+            builder.Register(c => c.Resolve<ChannelFactory<IService>>().CreateChannel()).As<IService>().UseWcfSafeRelease();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ Client/Controllers/InitController.cs
@@ -0,0 +1,25 @@
+﻿using Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Controllers
+{
+    public class InitController : Controller
+    {
+        readonly IService service;
+
+        public InitController(IService _service)
+        {
+            service = _service;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = service.RetornarCadena();
+            return View();
+        }
+    }
+}
